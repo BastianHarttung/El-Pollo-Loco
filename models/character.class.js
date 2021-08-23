ec1fe90af889854663fe111476441eb859d28bbc
BastianHarttung <info@harttung-fotografie.de>
2021-08-23 17:30:49 +0200

    world character

---
+++ models/character.class.js
@@ -0,0 +1,15 @@
+class Character extends MovableObject{
+    
+    constructor(){
+        super().loadImage('../img/2.Secuencias_Personaje-Pepe-corrección/1.IDLE/IDLE/I-1.png')
+    }
+
+
+    moveRight(){
+
+    }
+
+    jump(){
+
+    }
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,17 @@
+class MovableObject {
+    x =120;
+    y = 100;
+    img;
+    height=197;
+    width=100;
+
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveLeft() {
+                
+    }
+}
