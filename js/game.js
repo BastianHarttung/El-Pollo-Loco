312a93ef3c4115dbd8fa141198c214d84303e743
BastianHarttung <info@harttung-fotografie.de>
2021-08-30 12:08:00 +0200

    add Keyboard listener

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 /**
  * Define canvas and World on start
@@ -12,3 +13,32 @@ function init(){
     console.log('My Enemies are: ' ,world.enemies) */
 }
 
+document.addEventListener('keydown', (event) => { 
+    console.log('taste gedrückt', event.code);
+
+    if (event.code == 'ArrowRight') {
+        keyboard.RIGHT = true;
+    } if (event.code == 'ArrowLeft'){
+        keyboard.LEFT = true;
+    } if (event.code == 'ArrowUp'){
+        keyboard.LEFT = true;
+    } if (event.code == 'Space'){
+        keyboard.LEFT = true;
+    }
+    
+}, false);
+
+document.addEventListener('keyup', (event) => { 
+    console.log('taste losgelassen', event.code);
+
+    if (event.code == 'ArrowRight') {
+        keyboard.RIGHT = false;
+    } if (event.code == 'ArrowLeft'){
+        keyboard.LEFT = false;
+    } if (event.code == 'ArrowUp'){
+        keyboard.LEFT = false;
+    } if (event.code == 'Space'){
+        keyboard.LEFT = false;
+    }
+    
+}, false);
--- js/keyboard.class.js
+++ js/keyboard.class.js
@@ -1,36 +1,42 @@
-let left = false;
-let right = false;
-let up = false;
-let space = false;
+class Keyboard {
+    RIGHT = false;
+    LEFT = false;
+    UP = false;
+    SPACE = false;
+
+    constructor(){
+        
+      
+    }
+}
 
 
-document.addEventListener('keydown', (event) => {    
+/* document.addEventListener('keydown', (event) => {    
     
     console.log('taste gedrückt', event);
     
     var code = event.code;
 
-    if (code == 'ArrowRight') { 
-        event.repeat = false;             
+    if (code == 'ArrowRight') {                      
         right = true;
         world.moveRight(right);
         
     } if(code == 'ArrowLeft'){
         left = true;
         world.moveLeft();
-    }
+    } */
 
     // Alert the key name and key code on keydown
     /* alert(`Key pressed ${name} \r\n Key code value: ${code}`); */
     
-  }, false);
+/*   }, false);
 
   document.addEventListener('keyup', (event) => {    
     console.log('taste losgelassen', event)
     right = false;
     left = false;
     
-  }, false);
+  }, false); */
 
 
 
